6f01ccf52139554775eeaa7e83a7a070be952160
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-12 12:32:56 +0200

    feat: setup initial Spring Boot project

---
+++ src/main/java/se/backend1/pensionat/PensionatApplication.java
@@ -0,0 +1,13 @@
+package se.backend1.pensionat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PensionatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/backend1/pensionat/PensionatApplicationTests.java
@@ -0,0 +1,13 @@
+package se.backend1.pensionat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
