87cf6ca2c0f9dc1ee910c4341edfd8c669ec4d48
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-12 15:53:34 +0200

    feat: add Customer entity with validation and booking relation

--- src/main/java/se/backend1/pensionat/entity/Customer.java
+++ src/main/java/se/backend1/pensionat/entity/Customer.java
@@ -1,4 +1,42 @@
 package se.backend1.pensionat.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customers")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Namn krävs")
+    @Size(max = 100, message = "Namn får max vara 100 tecken")
+    private String name;
+
+    @NotBlank(message = "E-post krävs")
+    @Email(message = "Ogiltig e-postadress")
+    @Size(max = 100)
+    private String email;
+
+    @NotBlank(message = "Telefonnummer krävs")
+    @Size(max = 20)
+    private String phoneNumber;
+
+    @Size(max = 200)
+    private String address;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Booking> bookings;
 }
