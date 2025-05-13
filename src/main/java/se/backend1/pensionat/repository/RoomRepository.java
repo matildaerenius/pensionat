d58a0e82c788a12baf6546244262afed96c6bbf4
Dannyyari <danny_yari@yahoo.com>
2025-05-13 13:22:28 +0200

    Repo fix

--- src/main/java/se/backend1/pensionat/repository/RoomRepository.java
+++ src/main/java/se/backend1/pensionat/repository/RoomRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import se.backend1.pensionat.entity.Room;
 
+import java.util.List;
+
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+    List<Room> findByRoomType()
 
 }
