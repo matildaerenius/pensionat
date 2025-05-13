873212bcf7b2effb0daadb35df74be8c5f420a2a
Honey Bergstrom <phueng03735@gmail.com>
2025-05-13 13:20:15 +0200

    first commit

--- src/main/java/se/backend1/pensionat/entity/Room.java
+++ src/main/java/se/backend1/pensionat/entity/Room.java
@@ -1,4 +1,15 @@
 package se.backend1.pensionat.entity;
 
 public class Room {
+    //Annotera klassen med @entity och @table(name = "rooms")
+
+   // Lägg till primärnyckel: @id, @generatedvalue
+
+   // Lägg till fält, id, roomnr, enum?rumtyp? maxextrabeds
+
+    //Lägg till @onetomany(mappedBy = "room") till List<Booking>
+
+   // Setter och getter och kons
+
+    //Add an item
 }
