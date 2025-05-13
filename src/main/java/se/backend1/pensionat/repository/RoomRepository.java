6954a57946127dc0ab104f6ec6e627a5a2e00979
Dannyyari <danny_yari@yahoo.com>
2025-05-13 13:00:47 +0200

    JPA inlagd

--- src/main/java/se/backend1/pensionat/repository/RoomRepository.java
+++ src/main/java/se/backend1/pensionat/repository/RoomRepository.java
@@ -1,4 +1,9 @@
 package se.backend1.pensionat.repository;
 
-public interface RoomRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import se.backend1.pensionat.entity.Room;
+
+public interface RoomRepository extends JpaRepository<Room, Integer> {
+
 }
