37f2435be47b679f5dbf434d0817148fa83dabd6
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-14 13:47:21 +0200

    feat: add RoomService interface methods in RoomServiceImpl

--- src/main/java/se/backend1/pensionat/service/impl/RoomServiceImpl.java
+++ src/main/java/se/backend1/pensionat/service/impl/RoomServiceImpl.java
@@ -1,4 +1,33 @@
 package se.backend1.pensionat.service.impl;
 
-public class RoomServiceImpl {
+import se.backend1.pensionat.dto.RoomDto;
+import se.backend1.pensionat.service.RoomService;
+
+import java.util.List;
+
+public class RoomServiceImpl implements RoomService {
+    @Override
+    public RoomDto createRoom(RoomDto dto) {
+        return null;
+    }
+
+    @Override
+    public RoomDto updateRoom(Long id, RoomDto dto) {
+        return null;
+    }
+
+    @Override
+    public void deleteRoom(Long id) {
+
+    }
+
+    @Override
+    public RoomDto getRoomById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<RoomDto> getAllRooms() {
+        return List.of();
+    }
 }
