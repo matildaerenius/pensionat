ddf8782fe9f2a567041aa439037a2294215b6dc5
Dannyyari <danny_yari@yahoo.com>
2025-05-14 15:10:26 +0200

    Bort med kommentarer i CustomerRepo

--- src/main/java/se/backend1/pensionat/repository/CustomerRepository.java
+++ src/main/java/se/backend1/pensionat/repository/CustomerRepository.java
@@ -8,10 +8,10 @@ import java.util.List;
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     // Tillgång till Crud-metoder mot SQL.DB
 
-//                //Mer specifika metoder utanför JpaRepository
-//    List<Customer> findByLastName(String lastName);
-//    List<Customer> findByFirstName(String firstName);
-//    List<Customer> findByLastNameAndFirstName(String lastName, String firstName);
+                //Mer specifika metoder utanför JpaRepository
+     List<Customer> findByLastName(String lastName);
+     List<Customer> findByFirstName(String firstName);
+     List<Customer> findByLastNameAndFirstName(String lastName, String firstName);
 
 
 }
