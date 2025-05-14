074decdf78eb607abf161eb983881fa9ee0aaa26
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-14 13:12:31 +0200

    feat: add RoomService interface

--- src/main/java/se/backend1/pensionat/service/RoomService.java
+++ src/main/java/se/backend1/pensionat/service/RoomService.java
@@ -1,4 +1,18 @@
 package se.backend1.pensionat.service;
 
+import se.backend1.pensionat.dto.RoomDto;
+
+import java.util.List;
+
 public interface RoomService {
+
+    RoomDto createRoom(RoomDto dto);
+
+    RoomDto updateRoom(Long id, RoomDto dto);
+
+    void deleteRoom(Long id);
+
+    RoomDto getRoomById(Long id);
+
+    List<RoomDto> getAllRooms();
 }
