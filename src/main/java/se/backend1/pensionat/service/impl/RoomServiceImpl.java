9585fc7145dab47e8fcbf6e306001a68bddc0f3c
Honey Bergstrom <phueng03735@gmail.com>
2025-05-14 14:05:22 +0200

    Validering: extrasängar tillåts endast för dubbelrum

--- src/main/java/se/backend1/pensionat/service/impl/RoomServiceImpl.java
+++ src/main/java/se/backend1/pensionat/service/impl/RoomServiceImpl.java
@@ -2,12 +2,26 @@ package se.backend1.pensionat.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.backend1.pensionat.entity.Room;
+import se.backend1.pensionat.model.RoomType;
 import se.backend1.pensionat.repository.RoomRepository;
 import se.backend1.pensionat.service.RoomService;
 
 @Service
 public class RoomServiceImpl implements RoomService {
-    @Autowired
+   @Autowired
     private RoomRepository roomRepository;
 
+    public RoomServiceImpl(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+
+    }
+@Override
+    public Room saveRoom(Room room){
+        //If the room is not a DOUBLE room but still has extra beds set (maxExtraBeds > 0), then that’s not allowed.
+        if(room.getRoomType()!=RoomType.DOUBLE&& room.getMaxExtraBeds()>0){
+            throw new IllegalArgumentException("Extrasängar är endast tillåtna för dubbelrum");
+        }
+        return roomRepository.save(room);
+}
 }
