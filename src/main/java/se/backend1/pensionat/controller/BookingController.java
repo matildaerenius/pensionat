cae66366f78f7dd43926ffe37e4d7da8832631d1
Dannyyari <danny_yari@yahoo.com>
2025-05-14 09:11:24 +0200

    booking controller lagt in repo

--- src/main/java/se/backend1/pensionat/controller/BookingController.java
+++ src/main/java/se/backend1/pensionat/controller/BookingController.java
@@ -1,4 +1,40 @@
 package se.backend1.pensionat.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.backend1.pensionat.repository.BookingRepository;
+import se.backend1.pensionat.service.BookingService;
+
+@RestController
+@RequestMapping("/api/booking")
 public class BookingController {
+    private final BookingRepository repo;
+
+    public BookingController(BookingRepository repository) {
+        this.repo = repository;
+    }
+
+
+
+
 }
+
+
+/**
+ * Kustomiserade Querys
+ *
+ * två olika sätt att skriva
+ *
+ *  * @Modifying
+ *  * @Transactional
+ *  * @Query ("update X set y=:newVal where x=:oldVal")
+ *  public void updateVal(Sting newVal, String oldVal)
+ *
+ * @Modifying
+ * @Transactional
+ * @Query ("update X set y=?1 where x=?2")
+ * public void updateVal(@Param ("newVal")
+ * String newVal, @param("oldvVal) String oldVal)
+ *
+ */
--- src/main/java/se/backend1/pensionat/repository/RoomRepository.java
+++ src/main/java/se/backend1/pensionat/repository/RoomRepository.java
@@ -3,10 +3,12 @@ package se.backend1.pensionat.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import se.backend1.pensionat.entity.Room;
+import se.backend1.pensionat.model.RoomType;
 
 import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Integer> {
-    List<Room> findByRoomType()
+    List<Room> findByRoomType(RoomType roomType);
+    List <Room> findByBaseCapacityGreaterThanEqual(int baseCapacity);
 
 }
