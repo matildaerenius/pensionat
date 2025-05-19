46eda743272b18d828792d1a0851cf947403abe2
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-19 09:41:55 +0200

    feat: finish GlobalExceptionHandler and deleted ErrorDetails

--- src/main/java/se/backend1/pensionat/exception/GlobalExceptionHandler.java
+++ src/main/java/se/backend1/pensionat/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package se.backend1.pensionat.exception;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,14 +16,19 @@ public class GlobalExceptionHandler {
             InvalidBookingDatesException.class
     })
 
-    // TODO : Fixa denna
-    public String handleBusinessExceptions() {
-        return null;
+    public String handleBusinessExceptions(RuntimeException ex, Model model) {
+        model.addAttribute("errorTitle", "Ett fel har uppstått");
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error"; // Om vi har en html sida error.html
     }
 
-    // TODO : Fixa denna
+    /**
+     * Fångar alla andra oväntade fel som inte har specifika @ExceptionHandlers, typ nullpoint
+     */
     @ExceptionHandler(Exception.class)
-    public String handleOtherErrors() {
-        return null;
+    public String handleOtherErrors(Exception ex, Model model) {
+        model.addAttribute("errorTitle", "Oväntat fel");
+        model.addAttribute("errorMessage", "Något gick fel. Försök igen senare.");
+        return "error"; // Om vi har en html sida error.html, annars kanske vi bara kan logga felet
     }
 }
