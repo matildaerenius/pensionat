5db145b1a05ec8180471bf49a4136cb8eb524262
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-19 18:41:54 +0200

    refactor(BookingServiceImpl): cleaned up

--- src/main/java/se/backend1/pensionat/service/impl/BookingServiceImpl.java
+++ src/main/java/se/backend1/pensionat/service/impl/BookingServiceImpl.java
@@ -9,11 +9,9 @@ import se.backend1.pensionat.exception.BookingNotFoundException;
 import se.backend1.pensionat.exception.CustomerHasBookingsException;
 import se.backend1.pensionat.mapper.BookingMapper;
 import se.backend1.pensionat.repository.BookingRepository;
-import se.backend1.pensionat.repository.CustomerRepository;
 import se.backend1.pensionat.service.BookingService;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,56 +20,53 @@ import java.util.stream.Collectors;
 public class BookingServiceImpl implements BookingService {
 
     private final BookingRepository bookingRepository;
-    private final CustomerRepository customerRepository;
     private final BookingMapper bookingMapper;
 
 
     @Override
     public BookingDto createBooking(BookingDto dto) {
-        Booking bookinEntity = bookingMapper.toEntity(dto);
-        Booking saved= bookingRepository.save(bookinEntity);
+        Booking entity = bookingMapper.toEntity(dto);
+        Booking saved = bookingRepository.save(entity);
         return bookingMapper.toDto(saved);
     }
 
     @Override
     public BookingDto updateBooking(Long id, BookingDto dto) {
-        Booking exisitng= bookingRepository.findById(id)
-                .orElseThrow( () ->new BookingNotFoundException("Booking not found with ID" + id));
+        Booking existing = bookingRepository.findById(id)
+                .orElseThrow(() -> new BookingNotFoundException("Booking not found with ID" + id));
 
-        exisitng.setCheckIn(dto.getCheckIn());
-        exisitng.setCheckOut(dto.getCheckOut());
-        exisitng.setNumberOfGuests(dto.getNumberOfGuests());
+        existing.setCheckIn(dto.getCheckIn());
+        existing.setCheckOut(dto.getCheckOut());
+        existing.setNumberOfGuests(dto.getNumberOfGuests());
 
-        Booking saved= bookingRepository.save(exisitng);
-
+        Booking saved= bookingRepository.save(existing);
         return bookingMapper.toDto(saved);
     }
 
     @Override
     public void deleteBooking(Long id) {
-        Booking exisitng= bookingRepository.findById(id)
-                .orElseThrow( () ->new BookingNotFoundException("Booking not found with ID" + id));
-
-        LocalDate today = LocalDate.now();
+        Booking existing= bookingRepository.findById(id)
+                .orElseThrow(() -> new BookingNotFoundException("Booking not found with ID" + id));
 
         // Om checkOut är idag eller senare → pågående eller framtida bokning
-        if (!exisitng.getCheckOut().isBefore(today)) {
+        if (!existing.getCheckOut().isBefore(LocalDate.now())) {
             throw new CustomerHasBookingsException("Customer has bookings, cannot be removed");
         }
-        bookingRepository.delete(exisitng);
+        bookingRepository.delete(existing);
     }
 
     @Override
     public BookingDto getBookingById(Long id) {
-        Booking exisitng= bookingRepository.findById(id)
-                .orElseThrow( () ->new BookingNotFoundException("Booking not found with ID" + id));
-        return bookingMapper.toDto(exisitng);
+        Booking existing = bookingRepository.findById(id)
+                .orElseThrow(() -> new BookingNotFoundException("Booking not found with ID" + id));
+        return bookingMapper.toDto(existing);
     }
 
     @Override
     public List<BookingDto> getBookingsByCustomer(Long customerId) {
-        List<Booking> bookings = bookingRepository.findByCustomerId(customerId).stream().toList();
-        return bookings.stream().map(bookingMapper::toDto).collect(Collectors.toList());
+        return bookingRepository.findByCustomerId(customerId).stream()
+                .map(bookingMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -89,7 +84,9 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public List<BookingDto> getAllBookings() {
-        return bookingRepository.findAll().stream().map(bookingMapper::toDto).collect(Collectors.toList());
+        return bookingRepository.findAll().stream()
+                .map(bookingMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
