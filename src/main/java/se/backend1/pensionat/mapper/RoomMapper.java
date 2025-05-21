b4b46177a1170bd4131bc5077e2e549237178c99
robin <robin.oqvist@yh.nackademin.se>
2025-05-21 12:27:53 +0200

    flyttat layout under fragments och ändrat i RoomMapper capacity

--- src/main/java/se/backend1/pensionat/mapper/RoomMapper.java
+++ src/main/java/se/backend1/pensionat/mapper/RoomMapper.java
@@ -6,24 +6,50 @@ import se.backend1.pensionat.entity.Room;
 
 @Component
 public class RoomMapper {
-
-
-    // room till roomDTO
     public RoomDto toDto(Room room) {
+        if (room == null) return null;
+
         return RoomDto.builder()
                 .id(room.getId())
                 .roomNumber(room.getRoomNumber())
                 .roomType(room.getRoomType())
+                .capacity(room.getCapacity())
+                .allowExtraBeds(room.isAllowExtraBeds())
+                .maxExtraBeds(room.getMaxExtraBeds())
                 .build();
     }
 
-    // roomDTO till room
-    public Room toEntity(RoomDto roomDto) {
+    public Room toEntity(RoomDto dto) {
+        if (dto == null) return null;
+
         return Room.builder()
-                .id(roomDto.getId())
-                .roomNumber(roomDto.getRoomNumber())
-                .roomType(roomDto.getRoomType())
+                .id(dto.getId())
+                .roomNumber(dto.getRoomNumber())
+                .roomType(dto.getRoomType())
+                .capacity(dto.getCapacity())
+                .allowExtraBeds(dto.isAllowExtraBeds())
+                .maxExtraBeds(dto.getMaxExtraBeds())
                 .build();
     }
-
 }
+
+
+//    // room till roomDTO
+//    public RoomDto toDto(Room room) {
+//        return RoomDto.builder()
+//                .id(room.getId())
+//                .roomNumber(room.getRoomNumber())
+//                .roomType(room.getRoomType())
+//                .capacity().allowExtraBeds().maxExtraBeds().build();
+//    }
+//
+//    // roomDTO till room
+//    public Room toEntity(RoomDto roomDto) {
+//        return Room.builder()
+//                .id(roomDto.getId())
+//                .roomNumber(roomDto.getRoomNumber())
+//                .roomType(roomDto.getRoomType())
+//                .capacity().allowExtraBeds().maxExtraBeds().build();
+//    }
+//
+//}
